74a151bad97af2fc1dedb8fbc6dbbd7a5c202cf0
javabugmaker <100993166+javabugmaker@users.noreply.github.com>
2026-08-21 03:34:30 +0000

    v84 clean static typing import

--- web_report_v84.py
+++ web_report_v84.py
@@ -23,7 +23,6 @@ from collections.abc import Iterable
 from dataclasses import dataclass
 from datetime import date, datetime
 from pathlib import Path
-from typing import Any
 
 import numpy as np
 import pandas as pd
